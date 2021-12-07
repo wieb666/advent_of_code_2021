6f4c1a0b774304bf49be447c2f4842b67a8eb1c5
Wiebe <wiebe.kooistra@gmail.com>
2021-12-07 16:47:38 +0100

    day 7 code (no cheating)

---
+++ day7/mainp1.py
@@ -0,0 +1,18 @@
+import sys
+import numpy as np
+
+def main(fname):
+    with open(fname) as f:
+        pos = list(map(int, f.readlines()[0].split(",")))
+
+    lowest_fuel = None
+    for i in range(len(pos)):
+        fuel = [abs(x-i) for x in pos]
+        if not lowest_fuel:
+            lowest_fuel = sum(fuel)
+        if sum(fuel) < lowest_fuel:
+            lowest_fuel = sum(fuel)
+    print(lowest_fuel)
+
+if __name__ == "__main__":
+    main(sys.argv[1])
